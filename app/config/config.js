6dee6cfe2be5be43755446286ff0d943344ff28f
diahnkha <diahnurkhasanah5@gmail.com>
2022-12-28 11:01:08 +0700

    first commit

--- app/config/config.js
+++ app/config/config.js
@@ -5,6 +5,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     logging: false,
   },
@@ -13,6 +14,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     logging: false,
   },
@@ -21,6 +23,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     logging: false,
     pool: {
